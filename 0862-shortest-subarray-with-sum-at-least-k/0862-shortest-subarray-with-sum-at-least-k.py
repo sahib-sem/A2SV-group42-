f3690e112a4352cdcd6c9381425467a1086675a6
sahib-sem <107271456+sahib-sem@users.noreply.github.com>
2023-03-01 20:36:06 +0300

    Time: 1271 ms (91.76%), Space: 28.4 MB (45.12%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -3,7 +3,7 @@ class Solution:
         # the queue will be a monotollically increasing queue
         #res = min(res, i + 1 - d.popleft()[0])
         
-        queue = deque([(0 , 0)])
+        queue = deque([(-1 , 0)])
         n = len(nums)
         prefix_sum = 0
         min_length = n + 1
@@ -15,12 +15,12 @@ class Solution:
             while queue and prefix_sum - queue[0][1] >= k:
         
                 removed = queue.popleft()
-                min_length = min(min_length , end + 1 - removed[0])
+                min_length = min(min_length , end - removed[0])
                 
             while queue and prefix_sum <= queue[-1][1] :
                 queue.pop()
                 
-            queue.append((end + 1,prefix_sum))
+            queue.append((end ,prefix_sum))
         
                 
         return min_length if min_length != n + 1 else -1
