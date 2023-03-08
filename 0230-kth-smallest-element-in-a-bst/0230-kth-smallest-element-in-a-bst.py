734fa42f87ed68483d09f2cbd581fd8f474dae15
sahib-sem <107271456+sahib-sem@users.noreply.github.com>
2023-03-08 15:16:22 +0300

    Time: 47 ms (90.46%), Space: 18 MB (82.30%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        
+        ans = []
+        def smallest(root, ans):
+            
+            if root:
+                smallest(root.left, ans)
+                ans.append(root.val)
+                smallest(root.right, ans)
+        
+        smallest(root, ans)
+        return ans[k - 1]
+        
+        
+            
+                    
+                    
+            
+                
+        
