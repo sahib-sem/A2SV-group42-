ca8cd26236ff2ed81424de76e14f4793e0ad44dc
sahib-sem <107271456+sahib-sem@users.noreply.github.com>
2023-03-16 21:29:31 +0300

    Time: 34 ms (87.99%), Space: 13.9 MB (22.21%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,33 +5,26 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        answer=None
-        if not list2:
-            return list1
-        if not list1:
-            return list2
-        if list1 and list2 and list1.val<=list2.val:
-            answer=dummy=ListNode(list1.val)
-            list1=list1.next
-        else:
-            answer=dummy=list2
-            list2=list2.next
-        while list1 and list2:
-            if list1.val <list2.val:
-                dummy.next =ListNode(list1.val)
-                dummy=dummy.next
-                list1=list1.next
+        dummy = self.new_head = ListNode(0)
+        
+        def merge(h1 , h2):
+            if not h1:
+                self.new_head.next = h2
+                return 
+            if not h2:
+                self.new_head.next = h1
+                return h1
+            
+            if h1.val < h2.val:
+                self.new_head.next = h1
+                self.new_head = self.new_head.next
+                merge(h1.next , h2)
             else:
-                dummy.next=ListNode(list2.val)
-                list2=list2.next
-                dummy=dummy.next
-        while list1:
-            dummy.next=ListNode(list1.val)
-            dummy=dummy.next
-            list1=list1.next
-        while list2:
-            dummy.next=ListNode(list2.val)
-            dummy=dummy.next
-            list2=list2.next
-        return answer
+                self.new_head.next = h2
+                self.new_head = self.new_head.next
+                merge(h1, h2.next)
+                
+        merge(list1, list2)
+        
+        return dummy.next
         
