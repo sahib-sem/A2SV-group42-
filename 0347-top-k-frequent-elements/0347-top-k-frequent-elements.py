0b96c77164a7c04e651573bb5caebd291e7abe2d
sahib-sem <107271456+sahib-sem@users.noreply.github.com>
2023-03-30 00:12:59 +0300

    Time: 111 ms (41.93%), Space: 18.7 MB (42.44%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,16 +2,57 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
         # simple solution use a hash table 
+#         nums = Counter(nums)
+#         count = 0
+#         ans = []
+#         nums = dict(sorted(nums.items() , key = lambda x: -x[1] ))
+        
+#         for item in nums.keys():
+#             if count == k:
+#                 break
+#             ans.append(item)
+#             count += 1
+        
+#         return ans 
+       
         nums = Counter(nums)
-        count = 0
-        ans = []
-        nums = dict(sorted(nums.items() , key = lambda x: -x[1] ))
+        nums = [[val, key] for key, val in nums.items()]
+        self.ans = []
+        def quickselect(nums , start , end , k):
+            if end - start <= 0:
+                self.ans = nums[start:]
+                return 
+            
+            
+            pivot = nums[start][0]
+            write = start + 1
+            for read in range(start + 1, end + 1):
+                if nums[read][0] <= pivot:
+                    nums[write] , nums[read] = nums[read] , nums[write]
+                    write += 1
+            nums[start] , nums[write - 1] = nums[write - 1] , nums[start]
+            length = end - write + 2
+            
+            if length == k:
+                self.ans = nums[write - 1:]
+                return 
+            if k < length:
+                quickselect(nums, write, end , k )
+            else:
+                quickselect(nums, start, write - 2, k - length)
         
-        for item in nums.keys():
-            if count == k:
-                break
-            ans.append(item)
-            count += 1
+        quickselect(nums, 0 , len(nums) - 1, k)
         
-        return ans 
+        self.ans = [ans[1] for ans in self.ans]
+        
+        return self.ans
+        
+        
+            
+        
+        
+            
+            
+        
+        
         
