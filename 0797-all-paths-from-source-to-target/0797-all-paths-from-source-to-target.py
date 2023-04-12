e126f15b0da1948339d5bffa58b2c88bbf875854
sahib-sem <107271456+sahib-sem@users.noreply.github.com>
2023-04-12 15:33:01 +0300

    Time: 108 ms (27.38%), Space: 15.7 MB (72.88%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,20 +1,20 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         ds = len(graph) - 1
+        stack = [[0,0]]
         paths = []
-        stack = [[0, 0]]#rrent, last_neibour 
         
         while stack:
-            current, nei = stack[-1]
-            if current == ds and nei == 0:
-                paths.append(list(map(lambda x: x[0], stack)))
-                            
-            if nei < len(graph[current]):
+            
+            current , neighbour = stack[-1]
+            
+            if current == ds and neighbour == 0:
+                paths.append(list(map(lambda x: x[0] , stack)))
+            
+            if neighbour < len(graph[current]):
                 stack[-1][1] += 1
-                stack.append([graph[current][nei], 0])
+                stack.append([graph[current][neighbour] , 0])
             else:
                 stack.pop()
-            
-    
-        return paths 
-                
+        
+        return paths
