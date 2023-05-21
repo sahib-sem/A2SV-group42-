19b892c4f784fb5676e2181756b15d2e5def133b
Sahib Semahegn <107271456+sahib-sem@users.noreply.github.com>
2023-05-21 18:51:45 +0300

    Time: 1877 ms (76.73%), Space: 107.3 MB (57.58%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,25 +1,42 @@
+# UnionFind class
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.rank = [1] * size
+
+    # The find function here is the same as that in the disjoint set with path compression.
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    # The union function with union by rank
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
+
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+
+
+
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        graph = defaultdict(list)
+        uf = UnionFind(n)
+        for a , b in edges:
+            uf.union(a, b)
         
-        for edge in edges:
-            u, v = edge 
-            graph[u].append(v)
-            graph[v].append(u)
-        
-        def dfs(graph, curr, visited):
-            if curr == destination:
-                return True 
+        return uf.connected(source, destination)
             
-            visited.add(curr)
-            
-            for neighbour in graph[curr]:
-                if neighbour not in visited:
-                    if dfs(graph, neighbour , visited):
-                        return True
-            return False
         
-        return dfs(graph, source, set())
-                
             
         
