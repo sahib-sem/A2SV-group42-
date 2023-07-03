c50de758ac540f693e3f7331e61008e9990265c6
Sahib Semahegn <semahegnsahib@gmail.com>
2023-07-03 18:36:53 +0300

    Time: 48 ms (63.09%), Space: 16.6 MB (11.46%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.py
@@ -0,0 +1,40 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        #   226
+        # let make a backtracking function 
+        
+        memo = {}
+        if len(s) == 1 and s == "0":
+            return 0
+        
+        def dp(i):
+            
+            if i > len(s):
+                return 0
+            if i == len(s):
+                return 1
+            
+            
+            if i not in memo:
+                
+                
+                ans1, ans2 = dp(i + 1) , dp(i + 2)
+                
+                if s[i] == '0':
+                    ans1 = 0
+                    ans2 = 0
+                    
+                if i < len(s) - 1 and s[i+ 1] == '0':
+                    ans1 = 0
+                
+                if i < len(s) - 1 and int(s[i] + s[i + 1]) > 26:
+                    ans2 = 0
+                
+                memo[i] = ans1 + ans2
+                    
+            
+            return memo[i]
+        
+        return dp(0)
+    
+        
