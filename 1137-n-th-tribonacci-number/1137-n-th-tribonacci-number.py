0e83e9775400617ac86d7fd028170d50929cce3d
Sahib Semahegn <semahegnsahib@gmail.com>
2023-07-04 09:43:28 +0300

    Time: 45 ms (61.52%), Space: 16.3 MB (70.59%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,20 +1,33 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
         
-        memo = {}
+#         memo = {}
         
-        def trib(n):
-            if n <= 1:
-                return n
-            if n == 2:
-                return 1
+#         def trib(n):
+#             if n <= 1:
+#                 return n
+#             if n == 2:
+#                 return 1
             
-            if n not in memo:
-                memo[n] = trib(n-1) + trib(n-2) + trib(n-3)
+#             if n not in memo:
+#                 memo[n] = trib(n-1) + trib(n-2) + trib(n-3)
             
-            return memo[n]
+#             return memo[n]
         
-        return trib(n)
+#         return trib(n)
+    
+        # bottom up apprach 
+        if n <= 1:
+            return n
+        if n == 2:
+            return 1
+        arr = [0] * (n + 1)
+        arr[1] = arr[2] = 1
+        
+        for i in range(3, n + 1):
+            arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3]
+        
+        return arr[n]
 
         
         
