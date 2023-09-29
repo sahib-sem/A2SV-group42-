956ff469e8d2ac92fef7b5a78045e347f078aaf9
Sahib Semahegn <semahegnsahib@gmail.com>
2023-09-29 16:17:17 +0300

    Added solution for Longest Word in Dictionary

--- A2SV-group42-/longest-word-in-dictionary.py
+++ A2SV-group42-/longest-word-in-dictionary.py
@@ -1,32 +1,9 @@
 
-'''
-        
-        sort the input string using length in reverse order
-        
-        o(K^2 nlogn) 
-        word ->
-        
-        bruteforce will pass
-        
-        
-            w 5    o(k * n)
-            
-         o  4 
-         
-      r 3
-     
-    l 2
-    
-   d 1
-    
-'''
-
 class TrieNode:
     
     def __init__(self):
         
         self.children = {}
-        self.count = 0
         self.is_end = False
     
 
@@ -45,32 +22,8 @@ class Trie:
                 curr.children[char] = TrieNode()
                 
             curr = curr.children[char]
-            curr.count += 1
         
         curr.is_end = True
-    
-    def check_can_make_one_by_one(self, word:str) -> bool:
-        
-        n = len(word)
-        
-        curr = self.root
-        
-        for char in word: # world -> w 
-            
-            if char not in curr.children:
-                return False
-            
-            curr = curr.children[char]
-        
-            if curr.count < n:
-                return False
-            
-            if not curr.is_end:
-                return False
-            
-            n -= 1
-        
-        return True
             
         
         
@@ -78,25 +31,33 @@ class Trie:
 class Solution:
     def longestWord(self, words: List[str]) -> str:
         
-        words = sorted(words , key = lambda x : (len(x), x))
+        
         
         trie = Trie()
         
         for word in words:
             trie.add(word)
         
-        maximum_length = 0
+        
         ans = ""
         
-        for word in words:
-            
-            res = trie.check_can_make_one_by_one(word) 
-            
-            if res and len(word) > maximum_length:
-                maximum_length = len(word)
-                ans = word
+        stack = [(trie.root , "")]
         
+        while stack:
+            
+            current, word = stack.pop()
+            
+            for char, child in current.children.items():
+                
+                if child.is_end:
+                    next_word = word + char
+                    stack.append((child, next_word))
+                    
+                    if len(next_word) > len(ans) or (len(next_word) == len(ans) and next_word < ans):
+                        
+                        ans = next_word
         
         return ans
+                    
             
         
